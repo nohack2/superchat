b0e76bdcbbcae32856cd23aedae1fe337f103f57
kiran <186613317+nohack2@users.noreply.github.com>
2024-12-04 21:13:18 +0530

    risk: update eror checking based on rls for riskprofile table

--- src/app/api/checkRiskProfile/route.ts
+++ src/app/api/checkRiskProfile/route.ts
@@ -1,11 +1,9 @@
-import { createClient } from '@/utils/supabase/server';
+import { createClient} from '@/utils/supabase/server';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
   const supabase = await createClient();
   const { data: { user }, error: authError } = await supabase.auth.getUser();
-  console.log('user-----');
-  console.log(await supabase.auth.getUser());
 
   if (authError || !user) {
     return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
@@ -15,13 +13,19 @@ export async function GET() {
     .from('riskprofile')
     .select('profile_data')
     .eq('user_id', user.id)
-    .single();
+    .limit(1);
 
-  if (error && error.code === 'PGRST116') {
-    return NextResponse.json({ exists: false });
-  } else if (data) {
-    return NextResponse.json({ exists: true, profile: data.profile_data });
-  } else {
+  if (error) {
     return NextResponse.json({ error: 'Failed to check profile' }, { status: 500 });
+  } else if (data) {
+    // if rls is enabled without any policy on the table, you still get data as empty array [] irrespective of user having an entry in the riskprofile table
+    // So I am not sure why we dont get an error when rls policy is not configured instead of getting empty data which gives an illusion of working code.
+    let profileExists = false;
+    if (data.length > 0) {
+      profileExists = true;
+    }
+    return NextResponse.json({ exists: profileExists });
+  } else {
+    return NextResponse.json({ error: 'Unknown error' }, { status: 500 });
   }
 }
